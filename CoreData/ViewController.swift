9790525753f41ae6e63ed065abc3b03233f455bf
Jorge Casariego <jorgekasa@gmail.com>
2019-06-13 16:26:35 -0400

    Initial Commit

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Jorge Casariego on 6/13/19.
+//  Copyright © 2019 Jorge Casariego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
